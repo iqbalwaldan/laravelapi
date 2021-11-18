04d5b7aff8121cdf6bd8b7d12ed41e03f5e08bf8
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-11-19 02:57:57 +0700

    Membuat REST API Authorization

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//API route for register new user
+Route::post('/register', [AuthController::class, 'register']);
+
+//API route for login user
+Route::post('/login', [AuthController::class, 'login']);
+
+//Protecting Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+Route::get('/profile', function(Request $request) {return auth()->user();
+});
+
+// API route for logout user
+Route::post('/logout', [AuthController::class, 'logout']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
